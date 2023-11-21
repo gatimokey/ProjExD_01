ca3c13fb69be59bb35bc7820c658d34cf99ec35a
gatimonkey <c0a220335d@edu.teu.ac.jp>
2023-11-21 15:40:32 +0900

    3

--- pygame_image.py
+++ pygame_image.py
@@ -9,14 +9,14 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #1
     tori_img = pg.image.load("ex01/fig/3.png")
     tori_img = pg.transform.flip(tori_img,True,False)#2
-    tori_img = pg.transform.rotozoom(tori_img,10,1.0)#3
+    tori_list = [tori_img,pg.transform.rotozoom(tori_img,10,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])#4
-        screen.blit(tori_img,[0,0])
+        screen.blit(tori_list[1],[0,0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
